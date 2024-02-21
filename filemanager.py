35f0415245da8cf2a54c66d425a6d554e32f39d4
Argatrus <74654705+Argatrus@users.noreply.github.com>
2024-02-21 17:21:32 +0530

    Add files via upload

---
+++ filemanager.py
@@ -0,0 +1,43 @@
+import os
+
+
+def create_file(name: str):
+    open(name, 'w')
+
+
+def write_file(name: str, thing_to_write: str):
+    open(name, 'w').write(thing_to_write)
+
+
+def read_file(name: str):
+    return open(name, 'r').read()
+
+
+def delete_file(name: str):
+    os.remove(name)
+
+
+def create_folder(name: str):
+    os.mkdir(name)
+
+
+def delete_folder(name: str):
+    try:
+        os.rmdir(name)
+    except:
+        print(f'Error: {name} not empty')
+
+
+def rename_file(name: str, new_name: str):
+    os.rename(name, new_name)
+
+def clear_folder(name: str):
+    data = os.listdir(name)
+    for file in data:
+        os.remove(f'{name}/{file}')
+
+def delete_folderAndContents(name:str):
+    data = os.listdir(name)
+    for file in data:
+        os.remove(f'{name}/{file}')
+    os.rmdir('nice')
